8e4dbfaaa1dcfa5f9290a48f8a6793838c20349b
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-10-04 17:47:44 +0200

    Changing subject label size

--- App/basichtml/mobile.js
+++ App/basichtml/mobile.js
@@ -4,5 +4,7 @@ function ToMobile()
   $("#m-menu").css("display", "block");
   $("#menu").css("display", "none");
   $(".subject").css("width", "95%");
+  $("#subject-label").css("width", "100%");
+  $("#subject-label input").css("height", "80px");
 
 }
